b2769f66dc3dd19f6d838d3c9101fa45c334f99f
MkIINice <Mk1Hitch@gmail.com>
2023-01-02 17:26:16 +0200

    add main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CatsComponent } from './components/cats/cats.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatsComponent,
+    FilterComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule
